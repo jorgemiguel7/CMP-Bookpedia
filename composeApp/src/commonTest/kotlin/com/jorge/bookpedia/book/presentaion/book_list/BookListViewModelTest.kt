361e2039bd6a34b420b90787f85f60c6b8b48fc8
jorgecarvalho <jorge.carvalho@fretebras.com.br>
2025-03-08 17:14:49 -0300

    add unit tests to BookListViewmodel

--- composeApp/src/commonTest/kotlin/com/jorge/bookpedia/book/presentaion/book_list/BookListViewModelTest.kt
+++ composeApp/src/commonTest/kotlin/com/jorge/bookpedia/book/presentaion/book_list/BookListViewModelTest.kt
@@ -6,28 +6,63 @@ import com.jorge.bookpedia.book.presentation.book_list.BookListAction
 import com.jorge.bookpedia.book.presentation.book_list.BookListEffect
 import com.jorge.bookpedia.book.presentation.book_list.BookListViewModel
 import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.async
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class BookListViewModelTest {
 
+    private val testDispatcher = UnconfinedTestDispatcher()
     private val bookRepository: BookRepository = mockk()
-    private val bookListViewModel = BookListViewModel(bookRepository)
+    private val viewModel = BookListViewModel(bookRepository)
 
+    @BeforeTest
+    fun setup() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @AfterTest
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
 
     @Test
     fun `given valid action When onAction is called with OnBookClick Then NavigateToBookDetail effect is emitted`() =
         runTest {
-            val book = getBook()
+            val result = async(testDispatcher) { viewModel.effects.firstOrNull() }
+            viewModel.onAction(BookListAction.OnBookClick(getBook()))
 
-            val result = async { bookListViewModel.effects.firstOrNull() }
-            bookListViewModel.onAction(BookListAction.OnBookClick(book))
+            assertEquals(BookListEffect.NavigateToBookDetail(getBook()), result.await())
+        }
 
-            assertEquals(BookListEffect.NavigateToBookDetail(book), result.await())
+    @Test
+    fun `given valid action When onAction is called with OnSearchQueryChange Then state is updated with new query`() =
+        runTest {
+            val query = "Kotlin"
+
+            viewModel.onAction(BookListAction.OnSearchQueryChange(query))
+
+            assertEquals(query, viewModel.state.value.searchQuery)
+        }
+
+    @Test
+    fun `given valid action When onAction is called with OnTabSelected Then state is updated with new tab index`() =
+        runTest {
+            val index = 0
+
+            viewModel.onAction(BookListAction.OnTabSelected(index))
+
+            assertEquals(index, viewModel.state.value.selectedTabIndex)
         }
 
     private fun getBook() = Book(
